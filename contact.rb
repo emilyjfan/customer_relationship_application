6df303d339cabab1c6ae301595add005bb4226f2
EMILY FAN <emilyjustinefan@gmail.com>
2015-01-24 23:57:08 -0500

    updated .to_s string

--- contact.rb
+++ contact.rb
@@ -1,10 +1,16 @@
 class Contact
-	attr_accessor :id :first_name, :last_name, :email, :note
+	attr_accessor :id, :first_name, :last_name, :email, :note
 
-	def initialize(id, first_name, last_name, email, note)
+	def initialize(first_name, last_name, email, note)
 	@first_name = first_name
 	@last_name = last_name
 	@email = email
 	@note = note
 	end
+
+	def to_s 
+		"ID:#{@id.to_i + 1} First Name: #{@first_name} Last Name: #{@last_name} Email: #{@email} Note: #{@note}"
+	end
+
 end
+
